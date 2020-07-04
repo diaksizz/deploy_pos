2232cacdce13addd688d7bc0e7b8bc3594066727
Aditya Pratama <adityapratama2012@gmail.com>
2020-07-04 15:58:21 +0700

    up

--- mainapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Kategori)
-admin.site.register(Barang)
-admin.site.register(Transaksi)
-admin.site.register(RekapTransaksi)
